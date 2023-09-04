c78b0e64eec7fc0fa041b2858b8ab5e4a85f5d92
thomasprayon <thomasprayongowland@gmail.com>
2023-09-04 16:56:44 +0200

    Add information about Pokemons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-alert('Hello World!');
-let favoriteFood = 'Pasta Pesto'
-document.write(favoriteFood)
+let pokemonList = [
+    {
+        name: 'bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'charmander',
+        height: 0.6,
+        type:['fire']
+    },
+    {
+        name: 'squirtle' ,
+        height: 0.5 ,
+        type: ['water']
+    },
+    {
+        name: 'pidgey',
+        height: 0.3,
+        type: ['flying', 'normal']
+    },
+    {
+        name:'weddle',
+        height:0.3,
+        type: ['bug', 'poison']
+    }
+]
